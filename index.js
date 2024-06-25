eefb26c1f72a932a318af361550ff2c5d14018fa
coder7475 <robiulhossain7475@gmail.com>
2024-06-25 10:21:55 +0600

    feat: run main function

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// POST /api/users  - create a new user
-// app.post('/api/users')
+// create mon
+
+// Runs main function
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to MongoDB...");
+
+  // POST /api/users  - create a new user
+  // app.post('/api/users')
+}
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
